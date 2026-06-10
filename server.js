e5e8e64d04407d624801714722b4031b6341ed41
spzcespzce-coder <spzcespzce@gmail.com>
2026-06-10 18:57:16 +1200

    Add files via upload

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Simple moderation filter list (Add any words you want to block here in lowercase)
 const BANNED_WORDS = ['swear1', 'swear2', 'badword', 'ass', 'bitch', 'fuck']; 
 
 function moderateText(text) {
     let moderated = text;
     BANNED_WORDS.forEach(word => {
-        // Creates a regular expression to find the word safely case-insensitive
         const regex = new RegExp(`\\b${word}\\b`, 'gi');
         moderated = moderated.replace(regex, '■■■■');
     });
@@ -25,19 +23,14 @@ function moderateText(text) {
 io.on('connection', (socket) => {
     console.log('A user connected!');
 
-    // Listen for incoming message objects containing text and user profile data
     socket.on('chat message', (data) => {
-        // Moderate the text before sending it to anyone
         const cleanText = moderateText(data.text);
-        
         const packet = {
             text: cleanText,
             username: data.username,
             avatarColor: data.avatarColor
         };
-
-        // Broadcast the cleaned message package to everyone else
-        socket.broadcast.emit('chat message', packet);
+        io.emit('chat message', packet);
     });
 
     socket.on('disconnect', () => {
